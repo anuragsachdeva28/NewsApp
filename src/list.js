1a0d9ff136427e1ef525334b6c108a1f2a3a004e
Anurag Sachdeva <anurag.sach28@gmail.com>
2018-07-08 00:40:39 +0530

    First Commit

---
+++ src/list.js
@@ -0,0 +1,28 @@
+const list = [
+  {
+    title :'Tell me the truth without words',
+    url : 'http://kaloraat.com',
+    author : 'Ryan',
+    num_comments : 100,
+    points : 50,
+    objectID : 1
+  },
+  {
+    title :'Oh no, the candle is out',
+    url : 'http://kaloraat.com',
+    author : 'Zen',
+    num_comments : 100,
+    points : 50,
+    objectID : 2
+  },
+  {
+    title :'Writng anything just for name sake',
+    url : 'http://kaloraat.com',
+    author : 'Anurag',
+    num_comments : 100,
+    points : 50,
+    objectID : 3
+  }
+];
+
+export default list;
