4ff8c8e8422f3ba7611fe80194a5a6679921ef08
Anurag Sachdeva <anurag.sach28@gmail.com>
2018-08-30 21:42:15 +0530

    sort

--- src/list.js
+++ src/list.js
@@ -1,28 +1,30 @@
+// lets create some sample list of data
+
 const list = [
   {
-    title :'Tell me the truth without words',
-    url : 'http://kaloraat.com',
-    author : 'Ryan',
-    num_comments : 100,
-    points : 50,
-    objectID : 1
+    title: 'Tell me the truth without words',
+    url: 'http://kaloraat.com',
+    author: 'Ryan',
+    num_comments: 100,
+    points: 50,
+    objectID: 1
   },
   {
-    title :'Oh no, the candle is out',
-    url : 'http://kaloraat.com',
-    author : 'Zen',
-    num_comments : 100,
-    points : 50,
-    objectID : 2
+    title: 'Oh no! The candle is out',
+    url: 'http://kaloraat.com',
+    author: 'Zen',
+    num_comments: 50,
+    points: 20,
+    objectID: 2
   },
   {
-    title :'Writng anything just for name sake',
-    url : 'http://kaloraat.com',
-    author : 'Anurag',
-    num_comments : 100,
-    points : 50,
-    objectID : 3
+    title: 'The hard earned black belt',
+    url: 'http://kaloraat.com',
+    author: 'Ninja',
+    num_comments: 10,
+    points: 5,
+    objectID: 3
   }
-];
+]
 
 export default list;
